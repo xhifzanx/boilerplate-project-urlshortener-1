cc98d8c08f425a46fd0e535e5be50127ac1e7515
Syed Moinuddin Shibli <syed.m.shibli@et.apyl.com>
2023-08-23 13:28:39 +0530

    test

--- index.js
+++ index.js
@@ -91,7 +91,8 @@ app.get('/api/shorturl/:short_url?',async function(req, res) {
         console.error('Error:', error);
       });
     } else {
-      console.log('wrong format ' + req.params.short_url)
+      console.log(req.params)
+      console.log('wrong format ' + req)
       return res.json({"error":"Wrong format"})
     }
 
