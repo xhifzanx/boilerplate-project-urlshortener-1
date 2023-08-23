331d25b19c932c50f23dc0e1b9965b2663dcd6a2
Syed Moinuddin Shibli <syed.m.shibli@et.apyl.com>
2023-08-23 14:24:10 +0530

    bac

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.post('/api/shorturl', function(req, res) {
   })
 })
 
-app.get('/api/shorturl/:short_url?',async function(req, res) {
+app.get('/api/shorturl/:short_url',async function(req, res) {
   console.log(req.params)
   console.log(req.body)
   console.log(req.headers)
